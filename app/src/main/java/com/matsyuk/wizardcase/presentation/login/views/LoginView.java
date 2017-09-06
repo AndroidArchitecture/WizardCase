77a508dc7abb2503b69e988808d65acd9676d042
Eugene Matsyuk <matzuk2@mail.ru>
2017-09-06 07:26:48 +0300

    moxy refactoring

--- app/src/main/java/com/matsyuk/wizardcase/presentation/login/views/LoginView.java
+++ app/src/main/java/com/matsyuk/wizardcase/presentation/login/views/LoginView.java
@@ -1,13 +1,19 @@
 package com.matsyuk.wizardcase.presentation.login.views;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 /**
  * @author e.matsyuk
  */
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface LoginView extends MvpView {
 
+    @StateStrategyType(OneExecutionStateStrategy.class)
     void showSuccessLogin();
+    @StateStrategyType(OneExecutionStateStrategy.class)
     void showErrorLogin();
     void showProgress();
     void hideProgress();
--- app/src/main/java/com/matsyuk/wizardcase/presentation/registration/views/RegistrationView.java
+++ app/src/main/java/com/matsyuk/wizardcase/presentation/registration/views/RegistrationView.java
@@ -1,17 +1,23 @@
 package com.matsyuk.wizardcase.presentation.registration.views;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 /**
  * @author e.matsyuk
  */
+@StateStrategyType(AddToEndSingleStrategy.class)
 public interface RegistrationView extends MvpView {
 
     void showProgress();
     void hideProgress();
     void showErrorMail();
     void showPasswordError();
+    @StateStrategyType(OneExecutionStateStrategy.class)
     void showAuthError();
+    @StateStrategyType(OneExecutionStateStrategy.class)
     void showSuccess();
     void clearInputErrors();
 
